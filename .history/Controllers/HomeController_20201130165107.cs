28e66315bd17824c5ca3116e454fc998d8155fbf
steven-halla <69405488+steven-halla@users.noreply.github.com>
2020-11-30 16:52:51 -0800

    first commit

---
+++ .history/Controllers/HomeController_20201130165107.cs
@@ -0,0 +1,39 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+
+namespace Razor.Controllers     //be sure to use your own project's namespace!
+{
+    public class HomeController : Controller   //remember inheritance??
+    {
+        //for each route this controller is to handle:
+        [HttpGet]       //type of request
+        [Route("")]     //associated route string (exclude the leading /) local host 5000
+
+        //this will look for a vile name called Index
+        public ViewResult Index()
+        {
+            return View("index");
+        }
+
+        [HttpGet]
+        [Route("info")]
+
+        //this will look fora file name called info
+        //Views/Home/Index.cshtml
+        public ViewResult Info()
+        {
+            return View("Info");
+        }
+
+        [HttpGet]
+        [Route("elsewhere")]
+
+        //this will look for a file name called Elsewhere
+        public ViewResult Elsewhere()
+        {
+            return View("Index");
+        }
+
+
+    }
+}
